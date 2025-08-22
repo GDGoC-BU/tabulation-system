0637eb3184bbaf9c03e85e324948e1ac14f23538
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-22 20:10:56 +0800

    feat(backend/repositories): add ManagerRepository

---
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/repositories/ManagerRepository.java
@@ -0,0 +1,11 @@
+package com.michaelcanonizado.backend.repositories;
+
+import com.michaelcanonizado.backend.models.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ManagerRepository extends JpaRepository<Manager, UUID> {
+}
