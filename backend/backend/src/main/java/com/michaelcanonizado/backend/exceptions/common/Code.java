1424b03cca28d19d83ecbf8532baf8db42bdfc24
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-24 04:06:44 +0800

    docs(backend/exceptions): add comments to explain the structure of the ErrorResponse

--- backend/backend/src/main/java/com/michaelcanonizado/backend/exceptions/common/Code.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/exceptions/common/Code.java
@@ -1,13 +1,8 @@
 package com.michaelcanonizado.backend.exceptions.common;
 
+/* ONLY CHANGE THE PREFIX ENTITY IDENTIFIER */
 public enum Code {
-    COLLEGE_NOT_FOUND
-//    Other Examples:
-//    USER_NOT_FOUND,
-//    USER_ALREADY_EXISTS,
-//    PAYMENT_FAILED,
-//    INSUFFICIENT_BALANCE,
-//    BUSINESS_RULE_VIOLATION,
-//    RESOURCE_NOT_FOUND,
-//    INTERNAL_ERROR
+    COLLEGE_NOT_FOUND,
+    COLLEGE_ALREADY_EXIST,
+    INTERNAL_ERROR
 }
--- backend/backend/src/main/java/com/michaelcanonizado/backend/exceptions/common/ErrorResponse.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/exceptions/common/ErrorResponse.java
@@ -7,11 +7,27 @@ import java.time.ZonedDateTime;
 
 @Getter
 public class ErrorResponse {
+    /* HTTP status code. E.g: 404, 200, 500 */
     private int status;
+
+    /* HTTP status phrase: E.g: Not Found, Ok, Internal Server Error */
     private String error;
+
+    /* Machine-readable identifier for an error.
+       Can be used to programmatically handle the errors, especially in the
+       frontend. Also since the entities have a generic custom exception per
+       error type, this can be used to identify exactly what entity threw the
+       error.
+       E.g: COLLEGE_NOT_FOUND, CANDIDATE_ALREADY_EXIST, INTERNAL_ERROR */
     private Code code;
+
+    /* More detailed message of the error. Passed by the Caller */
     private String message;
+
+    /* URI where the error happened */
     private String path;
+
+    /* When the error occurred */
     private ZonedDateTime timestamp;
 
     public ErrorResponse(int status, String error, Code code, String message, String path) {
