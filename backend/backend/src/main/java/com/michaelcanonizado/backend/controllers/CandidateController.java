2d498a35bd28e845af4bbc37cb5bdd2ebec0bcd8
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-26 23:41:35 +0800

    feat(backend/[controllers,services,mappers]): add delete-candidate endpoint on Candidate

--- backend/backend/src/main/java/com/michaelcanonizado/backend/controllers/CandidateController.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/controllers/CandidateController.java
@@ -1,14 +1,15 @@
 package com.michaelcanonizado.backend.controllers;
 
 import com.michaelcanonizado.backend.dtos.candidate.CandidateCreateDTO;
+import com.michaelcanonizado.backend.dtos.candidate.CandidateSummaryDTO;
 import com.michaelcanonizado.backend.services.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("api/v1")
@@ -20,5 +21,11 @@ public class CandidateController {
     public ResponseEntity<Void> addCandidate(@RequestBody CandidateCreateDTO candidateCreateDTO) {
         service.addCandidate(candidateCreateDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/candidates/{id}")
+    public ResponseEntity<CandidateSummaryDTO> getCandidate(@PathVariable UUID id) {
+        CandidateSummaryDTO candidate = service.getCandidate(id);
+        return new ResponseEntity<>(candidate, HttpStatus.OK);
     }
 }
--- backend/backend/src/main/java/com/michaelcanonizado/backend/services/CandidateService.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/services/CandidateService.java
@@ -1,11 +1,17 @@
 package com.michaelcanonizado.backend.services;
 
 import com.michaelcanonizado.backend.dtos.candidate.CandidateCreateDTO;
+import com.michaelcanonizado.backend.dtos.candidate.CandidateSummaryDTO;
+import com.michaelcanonizado.backend.exceptions.common.ErrorCode;
+import com.michaelcanonizado.backend.exceptions.entity.EntityNotFoundException;
 import com.michaelcanonizado.backend.mappers.CandidateMapper;
 import com.michaelcanonizado.backend.models.Candidate;
+import com.michaelcanonizado.backend.models.College;
 import com.michaelcanonizado.backend.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class CandidateService {
@@ -17,5 +23,12 @@ public class CandidateService {
     public void addCandidate(CandidateCreateDTO candidateCreateDTO) {
         Candidate candidate = mapper.toEntity(candidateCreateDTO);
         repository.save(candidate);
+    }
+
+    public CandidateSummaryDTO getCandidate(UUID id) {
+        Candidate candidate = repository.findById(id).orElseThrow(() -> {
+            return new EntityNotFoundException("Candidate not found!", ErrorCode.CANDIDATE_NOT_FOUND);
+        });
+        return mapper.toSummaryDTO(candidate);
     }
 }
