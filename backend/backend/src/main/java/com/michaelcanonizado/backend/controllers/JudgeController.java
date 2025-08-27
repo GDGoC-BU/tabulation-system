a70fe15f60a5b32dc41d68fcc1dc7b465c5d1458
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-27 19:34:45 +0800

    feat(backend/[controllers,services]): add get-judge endpoint on Judge

--- backend/backend/src/main/java/com/michaelcanonizado/backend/controllers/JudgeController.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/controllers/JudgeController.java
@@ -7,10 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/api/v1")
@@ -22,5 +21,11 @@ public class JudgeController {
     private ResponseEntity<JudgeSummaryDTO> addJudge(@RequestBody JudgeCreateDTO judgeCreateDTO) {
         JudgeSummaryDTO judge = service.addJudge(judgeCreateDTO);
         return new ResponseEntity<>(judge, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/judges/{id}")
+    private ResponseEntity<JudgeSummaryDTO> getJudge(@PathVariable UUID id) {
+        JudgeSummaryDTO judge = service.getJudge(id);
+        return new ResponseEntity<>(judge, HttpStatus.OK);
     }
 }
--- backend/backend/src/main/java/com/michaelcanonizado/backend/services/JudgeService.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/services/JudgeService.java
@@ -2,11 +2,15 @@ package com.michaelcanonizado.backend.services;
 
 import com.michaelcanonizado.backend.dtos.judge.JudgeCreateDTO;
 import com.michaelcanonizado.backend.dtos.judge.JudgeSummaryDTO;
+import com.michaelcanonizado.backend.exceptions.common.ErrorCode;
+import com.michaelcanonizado.backend.exceptions.entity.EntityNotFoundException;
 import com.michaelcanonizado.backend.mappers.JudgeMapper;
 import com.michaelcanonizado.backend.models.Judge;
 import com.michaelcanonizado.backend.repositories.JudgeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class JudgeService {
@@ -18,6 +22,14 @@ public class JudgeService {
 
     public JudgeSummaryDTO addJudge(JudgeCreateDTO judgeCreateDTO) {
         Judge judge = repository.save(mapper.toEntity(judgeCreateDTO));
+        return mapper.toSummaryDTO(judge);
+    }
+
+    public JudgeSummaryDTO getJudge(UUID id) {
+        Judge judge = repository.findById(id).orElseThrow(() -> {
+            return new EntityNotFoundException("Judge not found!", ErrorCode.JUDGE_NOT_FOUND);
+        });
+
         return mapper.toSummaryDTO(judge);
     }
 }
