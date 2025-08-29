b816c5160afc68d27fd2a2163d29ed01d37dac92
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-29 21:35:23 +0800

    feat(backend/models): add Pageant and PageantStatus

---
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/models/Pageant.java
@@ -0,0 +1,28 @@
+package com.michaelcanonizado.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+public class Pageant extends Auditable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, updatable = false)
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PageantStatus status = PageantStatus.CLOSED;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+}
---
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/models/PageantStatus.java
@@ -0,0 +1,7 @@
+package com.michaelcanonizado.backend.models;
+
+public enum PageantStatus {
+    CLOSED,
+    ONGOING,
+    FINALIZING,
+}
