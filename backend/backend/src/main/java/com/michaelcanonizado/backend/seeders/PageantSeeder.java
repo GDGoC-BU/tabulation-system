a207bf9991d99b40de511af21806f73d79441f33
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-29 21:58:30 +0800

    feat(backend/seeders): add PageantSeeder

---
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/seeders/PageantSeeder.java
@@ -0,0 +1,27 @@
+package com.michaelcanonizado.backend.seeders;
+
+import com.michaelcanonizado.backend.models.Pageant;
+import com.michaelcanonizado.backend.repositories.PageantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Order(9)
+public class PageantSeeder implements CommandLineRunner {
+    private final PageantRepository repository;
+
+    @Autowired
+    public PageantSeeder(PageantRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(new Pageant("MMBU 2025-2026"));
+    }
+}
