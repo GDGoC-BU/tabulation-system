0e93e64651472062d5ab8c1d753d306db885a369
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-09-01 18:15:58 +0800

    feat(frontend/pages): add '/admin'

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,15 +1,15 @@
 import { LinkButton, LinkText } from '@/components/link'
 import { TextDisplay } from '@/components/text'
 
-function JudgeLogin() {
+function AdminLogin() {
   return (
     <div className='grid h-screen place-items-center'>
       <div className='flex flex-col items-center gap-4'>
         <div>
-          <TextDisplay>Judge</TextDisplay>
+          <TextDisplay>Admin</TextDisplay>
         </div>
         <div className='flex flex-col items-center gap-4'>
-          <LinkButton href='/judge/scoring'>Login</LinkButton>
+          <LinkButton href='/admin/dashboard'>Login</LinkButton>
           <LinkText href='/'>Home</LinkText>
         </div>
       </div>
@@ -17,4 +17,4 @@ function JudgeLogin() {
   )
 }
 
-export default JudgeLogin
+export default AdminLogin
