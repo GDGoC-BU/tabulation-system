8a6fd104fd50678602ab7a0b050fb033c332dc89
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-09-08 22:41:38 +0800

    feat(backend/configurations): add base SecurityConfiguration class and permit all requests

---
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/configurations/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.michaelcanonizado.backend.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().permitAll();
+                });
+        return httpSecurity.build();
+    }
+}
