0932a6a9afc01d9c3220c60eafc1af1706ad6d12
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-09-08 23:32:11 +0800

    feat(backend/utilities): add PageantContext to extract future pageantId claim from jwt. This reduces boilerplate code, I just DI this class instead of passing extracting the claim from the controllers and passing it to services

---
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/utilities/PageantContext.java
@@ -0,0 +1,35 @@
+package com.michaelcanonizado.backend.utilities;
+
+import com.michaelcanonizado.backend.exceptions.common.ErrorCode;
+import com.michaelcanonizado.backend.exceptions.entity.EntityNotFoundException;
+import com.michaelcanonizado.backend.models.Pageant;
+import com.michaelcanonizado.backend.repositories.PageantRepository;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.UUID;
+
+@Getter
+@RequestScope
+@Component
+public class PageantContext {
+    private final UUID id;
+
+    @Autowired
+    public PageantContext(PageantRepository repository) {
+        /* TEMPORARY HACK!
+           Get the pageant id from the jwt claim.
+           Also add a special custom exception for
+           this as this is simply not an entity search. */
+        this.id = repository
+                .findAll()
+                .stream()
+                .findFirst()
+                .map(Pageant::getId)
+                .orElseThrow(() -> {
+                    return new EntityNotFoundException("Pageant not found!", ErrorCode.ENTITY_NOT_FOUND);
+                });
+    }
+}
