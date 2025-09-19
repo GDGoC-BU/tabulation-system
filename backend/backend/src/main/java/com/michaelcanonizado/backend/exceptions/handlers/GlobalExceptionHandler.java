cbfe6ebf6b69bff335ec7674cce69b82992844ae
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-09-19 20:55:24 +0800

    fix(backend/exceptions): add AccessDeniedException handler

--- backend/backend/src/main/java/com/michaelcanonizado/backend/exceptions/handlers/GlobalExceptionHandler.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/exceptions/handlers/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -148,6 +149,24 @@ public class GlobalExceptionHandler {
                 status.value(),
                 status.getReasonPhrase(),
                 ErrorCode.INVALID_CREDENTIALS,
+                message,
+                request.getRequestURI()
+        );
+        return new ResponseEntity<>(response, status);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(
+            AccessDeniedException exception,
+            HttpServletRequest request
+    ) {
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        String message = "You are not allowed to do that! XD";
+
+        ErrorResponse response = new ErrorResponse(
+                status.value(),
+                status.getReasonPhrase(),
+                ErrorCode.ACCESS_DENIED,
                 message,
                 request.getRequestURI()
         );
