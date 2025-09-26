b9b1eb7810f4cb67293adaf937691f3974ff0c38
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-09-26 21:53:49 +0800

    chore(frontend/lib): axios, add Pageant-Id to request header if a pageant was selected

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { useAuthentication } from '@/features/authentication/store/use-authentication'
+import { useSelectedPageant } from '@/features/pageants/store/use-selected-pageant'
 
 const api = axios.create({
   baseURL: `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/api/v1`,
@@ -9,11 +10,22 @@ const api = axios.create({
   timeout: 10000,
 })
 
+/* Add token to authenticate request */
 api.interceptors.request.use((config) => {
   const { account, isAuthenticated } = useAuthentication.getState()
   if (isAuthenticated()) {
     config.headers.Authorization = `Bearer ${account?.token}`
   }
+  return config
+})
+
+/* Add pageant id, if available, to the request header  */
+api.interceptors.request.use((config) => {
+  const { pageant } = useSelectedPageant.getState()
+  if (!pageant) {
+    return config
+  }
+  config.headers['Pageant-Id'] = pageant.id
   return config
 })
 
