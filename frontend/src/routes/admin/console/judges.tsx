264c5dc20a5a642d693a8e1c3953a471798ec35a
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-09-26 22:15:31 +0800

    feat(frontend/features/judges): add judges table

--- frontend/src/routes/admin/console/judges.tsx
+++ frontend/src/routes/admin/console/judges.tsx
@@ -1,9 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Console from '@/components/console'
+import Table from '@/components/table'
+import { judgeTableColumns } from '@/features/judges/components/pageant-table-columns'
+import { useJudgesQuery } from '@/features/judges/hooks/use-judges-query'
 
 export const Route = createFileRoute('/admin/console/judges')({
-  component: RouteComponent,
+  component: AdminJudges,
 })
 
-function RouteComponent() {
-  return <div>Hello "/admin/console/judges"!</div>
+function AdminJudges() {
+  const { data } = useJudgesQuery()
+
+  return (
+    <Console>
+      <Console.Header>
+        <Console.Header.Title>Judges</Console.Header.Title>
+      </Console.Header>
+      <Console.Content>
+        <Table.Data columns={judgeTableColumns} data={data ?? []} />
+      </Console.Content>
+    </Console>
+  )
 }
