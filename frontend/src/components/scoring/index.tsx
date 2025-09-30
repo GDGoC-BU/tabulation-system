066344d98c19b4d141b8662beb315ff43bc2ebcc
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-09-30 14:51:31 +0800

    feat(frontend[/routes/judge/scoring,/components/scoring]): add Scoring compound components

--- frontend/src/components/scoring/index.tsx
+++ frontend/src/components/scoring/index.tsx
@@ -1,4 +1,4 @@
-import { TextBody, TextHeading } from '../text'
+import { TextBody, TextHeading, textBodyClassName } from '../text'
 import type { ComponentClassNameAndChildrenProp } from '@/types'
 import { cn } from '@/lib/utils'
 
@@ -30,8 +30,108 @@ function Content({ className, children }: ComponentClassNameAndChildrenProp) {
   return <div className={cn('grow', className)}>{children}</div>
 }
 
+function TabsFacade({
+  className,
+  children,
+}: ComponentClassNameAndChildrenProp) {
+  return (
+    <div
+      className={cn(
+        'flex flex-col gap-2 max-w-[1080px] w-full mx-auto',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+function TabsFacadeList({
+  className,
+  children,
+}: ComponentClassNameAndChildrenProp) {
+  return (
+    <div
+      className={cn(
+        'bg-muted text-muted-foreground inline-flex h-9 w-full items-center justify-center rounded-lg p-[3px]',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+function TabsFacadeListTrigger({
+  className,
+  children,
+  active = false,
+}: ComponentClassNameAndChildrenProp & { active?: boolean }) {
+  const activeClassName =
+    'bg-background text-foreground dark:border-input dark:bg-input/30 dark:text-muted-foreground shadow-sm'
+
+  return (
+    <div
+      className={cn(
+        "text-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        textBodyClassName,
+        active && activeClassName,
+        className,
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+function TabsFacadeBody({
+  className,
+  children,
+}: ComponentClassNameAndChildrenProp) {
+  return (
+    <div className="bg-muted rounded-lg p-[3px] grow w-full">
+      <div
+        className={cn(
+          'bg-background flex flex-col gap-2 p-4 rounded-md',
+          className,
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+function TabsFacadeBodyTitle({
+  className,
+  children,
+}: ComponentClassNameAndChildrenProp) {
+  return <TextHeading className={cn('', className)}>{children}</TextHeading>
+}
+
+function TabsFacadeBodyDescription({
+  className,
+  children,
+}: ComponentClassNameAndChildrenProp) {
+  return <TextBody className={cn('', className)}>{children}</TextBody>
+}
+
+function TabsFacadeBodyContent({
+  className,
+  children,
+}: ComponentClassNameAndChildrenProp) {
+  return (
+    <div className={cn('grid grid-cols-2 gap-4', className)}>{children}</div>
+  )
+}
+
 Header.Title = Title
 Header.Sub = Sub
 Scoring.Header = Header
+TabsFacadeList.Trigger = TabsFacadeListTrigger
+TabsFacade.List = TabsFacadeList
+TabsFacadeBody.Title = TabsFacadeBodyTitle
+TabsFacadeBody.Description = TabsFacadeBodyDescription
+TabsFacadeBody.Content = TabsFacadeBodyContent
+TabsFacade.Body = TabsFacadeBody
+Scoring.TabsFacade = TabsFacade
 Scoring.Content = Content
 export default Scoring
--- frontend/src/routes/judge/scoring/index.tsx
+++ frontend/src/routes/judge/scoring/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { TabsTrigger } from '@radix-ui/react-tabs'
-import { useEffect } from 'react'
+import { TabsContent, TabsTrigger } from '@radix-ui/react-tabs'
+import { useEffect, useState } from 'react'
+import type { Segments } from '@/features/segments/schemas'
 import { useAuthenticationStore } from '@/features/authentication/store/use-authentication-store'
 import { usePageantQuery } from '@/features/pageants/hooks/use-pageant-query'
 import Scoring from '@/components/scoring'
@@ -8,22 +9,38 @@ import { Tabs, TabsList } from '@/components/ui/tabs'
 import { useSelectedPageantIdStore } from '@/features/pageants/store/use-selected-pageant-id-store'
 import { useOngoingPhaseQuery } from '@/features/phases/hooks/use-ongoing-phase-query'
 import { TextDisplay } from '@/components/text'
+import { useSegmentsQuery } from '@/features/segments/hooks/use-segments-query'
+import { phaseSegmentStatusValue } from '@/schemas'
 
 export const Route = createFileRoute('/judge/scoring/')({
   component: JudgeScoring,
 })
 
 function JudgeScoring() {
+  const [currentSegments, setCurrentSegments] = useState<Segments>([])
   const { account, getAssignedPageantId } = useAuthenticationStore()
   const { data: assignedPageant } = usePageantQuery(getAssignedPageantId())
   const { setSelectedPageantId } = useSelectedPageantIdStore()
   const { data: currentPhase } = useOngoingPhaseQuery()
+  /* Temporary hack! Backend can return segments for a phase */
+  const { data: allSegments } = useSegmentsQuery()
 
   useEffect(() => {
     if (assignedPageant) {
       setSelectedPageantId(assignedPageant.id)
     }
   }, [assignedPageant])
+
+  useEffect(() => {
+    if (allSegments && currentPhase) {
+      const filtered = allSegments.filter((segment) => {
+        if (segment.phase.id === currentPhase.id) {
+          return segment
+        }
+      })
+      setCurrentSegments(filtered)
+    }
+  }, [allSegments, currentPhase])
 
   return (
     <Scoring>
@@ -33,11 +50,37 @@ function JudgeScoring() {
         <Scoring.Header.Sub>Welcome, {account?.username}</Scoring.Header.Sub>
       </Scoring.Header>
       <Scoring.Content>
-        <Tabs>
-          <TabsList>
-            <TabsTrigger value=""></TabsTrigger>
-          </TabsList>
-        </Tabs>
+        <Scoring.TabsFacade>
+          <Scoring.TabsFacade.List>
+            {currentSegments.map((segment) => {
+              return (
+                <Scoring.TabsFacade.List.Trigger
+                  active={
+                    segment.status === phaseSegmentStatusValue.enum.ONGOING
+                  }
+                >
+                  {segment.name}
+                </Scoring.TabsFacade.List.Trigger>
+              )
+            })}
+          </Scoring.TabsFacade.List>
+          <Scoring.TabsFacade.Body>
+            <Scoring.TabsFacade.Body.Title>
+              Swimwear
+            </Scoring.TabsFacade.Body.Title>
+            <Scoring.TabsFacade.Body.Description>
+              Enter scores for each contestant in the segment
+            </Scoring.TabsFacade.Body.Description>
+            <Scoring.TabsFacade.Body.Content>
+              <div className="p-4 rounded-lg border">1</div>
+              <div className="p-4 rounded-lg border">1</div>
+              <div className="p-4 rounded-lg border">2</div>
+              <div className="p-4 rounded-lg border">2</div>
+              <div className="p-4 rounded-lg border">3</div>
+              <div className="p-4 rounded-lg border">3</div>
+            </Scoring.TabsFacade.Body.Content>
+          </Scoring.TabsFacade.Body>
+        </Scoring.TabsFacade>
       </Scoring.Content>
     </Scoring>
   )
