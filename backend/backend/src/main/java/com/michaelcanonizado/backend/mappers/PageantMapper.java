766afd287eab8c5b63bb68fb1847ad73d744ba33
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-10-05 00:18:08 +0800

    fix(backend/mappers): properly serialize pageant.status

--- backend/backend/src/main/java/com/michaelcanonizado/backend/mappers/PageantMapper.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/mappers/PageantMapper.java
@@ -19,7 +19,7 @@ public interface PageantMapper {
        Pageant toEntity(PageantSummaryDTO pageantSummaryDTO); */
     @Mapping(target = "status", qualifiedByName = "mapStatus")
     PageantSummaryDTO toSummaryDTO(Pageant pageant);
-
+    @Mapping(target = "status", qualifiedByName = "mapStatus")
     PageantHierarchyDTO toHierarchyDTO(Pageant pageant);
 
     void updateEntityFromDTO(@MappingTarget Pageant pageant, PageantUpdateDTO pageantUpdateDTO);
