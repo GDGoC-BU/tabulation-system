92a4ef04c6f8296a338e2edf73925c530acebee4
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-11-05 15:59:07 +0800

    fix(frontend/features/navigation): show loading screen when logging out to hide UI teardown

--- frontend/src/features/navigation/components/judge/horizontal.tsx
+++ frontend/src/features/navigation/components/judge/horizontal.tsx
@@ -1,10 +1,14 @@
 import { useNavigate } from '@tanstack/react-router'
 import { LogOut } from 'lucide-react'
+import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { useAuthenticationStore } from '@/features/authentication/store/use-authentication-store'
 import { useSelectedPageantIdStore } from '@/features/pageants/store/use-selected-pageant-id-store'
+import { TextBody } from '@/components/text'
+import { cn } from '@/lib/utils'
 
 export default function Horizontal() {
+  const [isLoggingOut, setIsLoggingOut] = useState(true)
   const { logout } = useAuthenticationStore((state) => state)
   const { setSelectedPageantId: setPageantId } = useSelectedPageantIdStore(
     (state) => state,
@@ -12,21 +16,31 @@ export default function Horizontal() {
   const navigation = useNavigate()
 
   function logoutAccount() {
-    navigation({ to: '/' })
-    /* Add short delay so that UI is maintained. Navigation might take
-       sometime for the redirect page to load. This prevents UI teardown */
+    setIsLoggingOut(true)
+    logout()
+    setPageantId(null)
     setTimeout(() => {
-      logout()
-      setPageantId(null)
-    }, 2000)
+      setIsLoggingOut(false)
+      navigation({ to: '/' })
+    }, 1500)
   }
 
   return (
-    <div className="absolute right-4 top-4">
-      <Button onClick={logoutAccount}>
-        <LogOut />
-        Logout
-      </Button>
-    </div>
+    <>
+      <div className="absolute right-4 top-4">
+        <Button onClick={logoutAccount}>
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+      <div
+        className={cn(
+          'inset-0 z-[9999] bg-background grid place-items-center',
+          isLoggingOut ? 'absolute' : 'hidden',
+        )}
+      >
+        <TextBody>Logging out...</TextBody>
+      </div>
+    </>
   )
 }
