73361f0c6757f67b85fb1ddcbc4a70d5fd936445
Michael Rey Belga <mrab2023-5629-60654@bicol-u.edu.ph>
2025-11-24 20:30:37 +0800

    chore(backend/seeders): add MMBU 2025 data judge seeder

--- backend/backend/src/main/java/com/michaelcanonizado/backend/seeders/JudgeSeeder.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/seeders/JudgeSeeder.java
@@ -29,22 +29,22 @@ public class JudgeSeeder implements DatabaseSeeder {
 
         List<Judge> judges = Arrays.asList(
                 /* Closed-Door Interview Judges */
-                new Judge("a","", "James Edward","Tambobong", Honorific.MR, 1, pageant),
-                new Judge("b","", "Rhondon","Ricafort", Honorific.MR, 2, pageant),
-                new Judge("c","", "Neal","Llanto", Honorific.DR, 3, pageant),
-                new Judge("d","", "John Paul","Regaspi", Honorific.MX, 4, pageant),
-                new Judge("e","", "Phoebe Kate","Calisin", Honorific.MS, 5, pageant),
+                new Judge("JUDGE_1","Tambobong_2025", "James Edward","Tambobong", Honorific.MR, 1, pageant),
+                new Judge("JUDGE_2","Ricafort_2025", "Rhondon","Ricafort", Honorific.MR, 2, pageant),
+                new Judge("JUDGE_3","Llanto_2025", "Neal","Llanto", Honorific.DR, 3, pageant),
+                new Judge("JUDGE_4","Regaspi_2025", "John Paul","Regaspi", Honorific.MX, 4, pageant),
+                new Judge("JUDGE_5","Calisin_2025", "Phoebe Kate","Calisin", Honorific.MS, 5, pageant),
 
                 /* Coronation Night Judges */
-                new Judge("f","", "Patrixia Sherly","Santos", Honorific.ATTY, 6, pageant),
-                new Judge("g","", "Richard","Milante", Honorific.MR, 7, pageant),
-                new Judge("h","", "Abelardo Billy","Balin", Honorific.MR, 8, pageant),
-                new Judge("i","", "Juan","Sarte", Honorific.MR, 9, pageant),
-                new Judge("j","", "Jorim","Sumangid", Honorific.MX, 10, pageant),
-                new Judge("k","", "Alexandra Krishna","Orino", Honorific.MS, 11, pageant),
-                new Judge("l","", "Gabriel","Aguirre", Honorific.MR, 12, pageant),
-                new Judge("m","", "Margarette Siat","Briton", Honorific.MS, 13, pageant),
-                new Judge("n","", "Eileen Kae","Relao", Honorific.MS, 14, pageant)
+                new Judge("JUDGE_6","Santos_2025", "Patrixia Sherly","Santos", Honorific.ATTY, 6, pageant),
+                new Judge("JUDGE_7","Milante_2025", "Richard","Milante", Honorific.MR, 7, pageant),
+                new Judge("JUDGE_8","Balin_2025", "Abelardo Billy","Balin", Honorific.MR, 8, pageant),
+                new Judge("JUDGE_9","Sarte_2025", "Juan","Sarte", Honorific.MR, 9, pageant),
+                new Judge("JUDGE_10","Sumangid_2025", "Jorim","Sumangid", Honorific.MX, 10, pageant),
+                new Judge("JUDGE_11","Orino_2025", "Alexandra Krishna","Orino", Honorific.MS, 11, pageant),
+                new Judge("JUDGE_12","Aguirre_2025", "Gabriel","Aguirre", Honorific.MR, 12, pageant),
+                new Judge("JUDGE_13","Briton_2025", "Margarette Siat","Briton", Honorific.MS, 13, pageant),
+                new Judge("JUDGE_14","Relao_2025", "Eileen Kae","Relao", Honorific.MS, 14, pageant)
         );
 
         judges.forEach(judge -> {
