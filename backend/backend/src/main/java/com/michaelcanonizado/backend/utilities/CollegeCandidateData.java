440fde07983f600246dbb5a5bd0df288853ef81e
Derek P <pajarin.derek.b@gmail.com>
2025-11-24 20:26:17 +0800

    chore(backend/utilities): add MMBU 2025 candidate data to CollegeCandidateData

--- backend/backend/src/main/java/com/michaelcanonizado/backend/utilities/CollegeCandidateData.java
+++ backend/backend/src/main/java/com/michaelcanonizado/backend/utilities/CollegeCandidateData.java
@@ -43,8 +43,8 @@ public class CollegeCandidateData {
                     "BUPC",
                     "Bicol University Polangui Campus",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Lea Jane","Torre", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("James","Sumayao", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -52,8 +52,8 @@ public class CollegeCandidateData {
                     "BUIPESR",
                     "Bicol University Institute of Physical Education, Sports, and Recreation",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Ziha Khrystell Shanice","Boncodin", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Piolo KR","Kawata", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -61,8 +61,8 @@ public class CollegeCandidateData {
                     "BUCSSP",
                     "Bicol University College of Social Sciences and Philosophy",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Rey-Anne","Oserin", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Lemuel Dave","Azaña", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -70,8 +70,8 @@ public class CollegeCandidateData {
                     "BUCBEM",
                     "Bicol University College of Business, Economics, and Management",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Cassandra Sofia","Tapia", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Lex Harvey","Revale", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -79,8 +79,8 @@ public class CollegeCandidateData {
                     "BUCL",
                     "Bicol University College of Law",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Julienne Zen","Vicente", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Austin Matthew","Olila", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -88,8 +88,8 @@ public class CollegeCandidateData {
                     "BUJMRIGD",
                     "Bicol University Jesse M Robredo Insitute of Governance and Development",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Annika","Maneja", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Kristian Carlo","Celadiña", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -97,8 +97,8 @@ public class CollegeCandidateData {
                     "BUIDeA",
                     "Bicol University Institute of Design and Architecture",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Risel","Narvaja", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Patrick Jed Niño","Alfante", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -106,8 +106,8 @@ public class CollegeCandidateData {
                     "BUCENG",
                     "Bicol University College of Engineering",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Sarah Mae","Gimena", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("George","Angeles", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -115,8 +115,8 @@ public class CollegeCandidateData {
                     "BUGuinobatan",
                     "Bicol University Guinobatan Campus",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Hannah","Bejison", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Rene Rey","Morota", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -124,8 +124,8 @@ public class CollegeCandidateData {
                     "BUTC",
                     "Bicol University Tabaco Campus",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Princess Mae","Perez", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Roberto","Cardiño", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -133,8 +133,8 @@ public class CollegeCandidateData {
                     "BUCE",
                     "Bicol University College of Education",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Mikylla Janelle","Imperial", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Lloyd Anthony","Bertes", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -142,8 +142,8 @@ public class CollegeCandidateData {
                     "BUGubat",
                     "Bicol University Gubat Campus",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Jesryl","Espantaleon", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Elvin","Ofalsa", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -151,8 +151,8 @@ public class CollegeCandidateData {
                     "BUCIT",
                     "Bicol University College of Industrial Technology",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Pearl Amirey","Espinili", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Aries Carl","Aycardo", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -160,8 +160,8 @@ public class CollegeCandidateData {
                     "BUCM",
                     "Bicol University College of Medicine",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Stephany Mae","Chi", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Jon","Perez", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -169,8 +169,8 @@ public class CollegeCandidateData {
                     "BUCN",
                     "Bicol University College of Nursing",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Kim Vianice Mae","Pilarte", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Jorick","Gustuir", CandidateGender.MALE, 20)
                     )
             ),
             new CollegeTemp(
@@ -178,8 +178,8 @@ public class CollegeCandidateData {
                     "BUCS",
                     "Bicol University College of Science",
                     List.of(
-                            new CandidateTemp("","", CandidateGender.FEMALE, 20),
-                            new CandidateTemp("","", CandidateGender.MALE, 20)
+                            new CandidateTemp("Daisy","Camu", CandidateGender.FEMALE, 20),
+                            new CandidateTemp("Austin","Fennis", CandidateGender.MALE, 20)
                     )
             )
     );
