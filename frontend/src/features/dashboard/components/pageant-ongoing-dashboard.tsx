7fa676d182e646d71633332f26e71de93972d91e
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2026-01-19 15:09:42 +0800

    refactor(frontend/features/dashboard): redesign the dashboard ui

--- frontend/src/features/dashboard/components/pageant-ongoing-dashboard.tsx
+++ frontend/src/features/dashboard/components/pageant-ongoing-dashboard.tsx
@@ -1,114 +1,118 @@
+import { useQuery } from '@tanstack/react-query'
 import { useMemo } from 'react'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
-import type { Phases } from '@/features/phases/schemas'
-import type { Segments } from '@/features/segments/schemas'
-import { TextBody } from '@/components/text'
-import determineNextPhaseSegmentStatusAction from '@/features/state-machine/lib/determine-next-phase-segment-status-action'
+import type { SegmentHierarchy } from '@/features/segments/schemas'
+import type { PhaseHierarchy } from '@/features/phases/schemas'
 import { useSelectedPageant } from '@/features/pageants/hooks/use-selected-pageant'
-import useStatusChangeMutate from '@/features/state-machine/hooks/use-status-change-mutate'
+import pageantHierarchyQueryOptions from '@/features/pageants/query-options/pageant-hierarchy-query-options'
+import { TextBody, TextSub } from '@/components/text'
+import { phaseSegmentStatusValue } from '@/schemas'
 import ConfirmDialog from '@/components/confirm-dialog'
-import phasesQueryOptions from '@/features/phases/query-options/phases-query-options'
-import segmentsQueryOptions from '@/features/segments/query-options/segments-query-options'
 
-function PhaseSegmentTable({
-  phases,
-  segments,
-}: {
-  phases: Phases
-  segments: Segments
-}) {
+function Badge({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      {phases.map((phase) => {
-        return (
-          <div key={phase.id} className="pb-8 flex flex-col gap-4">
-            <div key={phase.id} className="flex flex-row gap-4 justify-between">
-              <div className="flex flex-row gap-2">
-                <TextBody>{phase.sequence}</TextBody>
-                <TextBody>{phase.name}</TextBody>
-              </div>
-              <TextBody>{phase.status}</TextBody>
-            </div>
-            {segments.map((segment) => {
-              if (segment.phase.id === phase.id) {
-                return (
-                  <div
-                    key={segment.id}
-                    className="ml-8 flex flex-row gap-4 justify-between"
-                  >
-                    <div className="flex flex-row gap-2">
-                      <TextBody>
-                        {phase.sequence}.{segment.sequence}
-                      </TextBody>
-                      <TextBody>{segment.name}</TextBody>
-                    </div>
-                    <TextBody>{segment.status}</TextBody>
-                  </div>
-                )
-              }
-            })}
-          </div>
-        )
-      })}
+    <div className="border size-fit px-2 py-1 rounded-full flex flex-row gap-1 items-center">
+      <TextSub className="text-foreground">{children}</TextSub>
     </div>
   )
 }
 
-export default function PageantOngoingDashboard() {
-  const { data: selectedPageant } = useSelectedPageant()
-  const { data: phases } = useQuery(phasesQueryOptions())
-  const { data: segments } = useQuery(segmentsQueryOptions())
-  const { mutateAsync: changeState } = useStatusChangeMutate()
-  const queryClient = useQueryClient()
+function SegmentCard({
+  segment,
+}: {
+  segment: SegmentHierarchy
+  isDisabled: boolean
+}) {
+  return (
+    <div className="grow p-6 border rounded-lg flex flex-col items-center gap-2">
+      <div className="flex flex-col gap-1 items-center">
+        <TextBody>{segment.name}</TextBody>
+        <Badge>{segment.status}</Badge>
+      </div>
+      <div className="flex flex-col gap-2 hover:cursor-pointer">
+        <TextSub>View Leaderboard</TextSub>
+      </div>
+    </div>
+  )
+}
 
-  /* Sort phases and segments just incase */
-  const sortedPhases = useMemo(() => {
-    if (!phases) return []
-    return [...phases].sort((a, b) => a.sequence - b.sequence)
-  }, [phases])
-  const sortedSegments = useMemo(() => {
-    if (!segments) return []
-    return [...segments].sort((a, b) => a.sequence - b.sequence)
-  }, [segments])
+function PhaseCard({
+  phase,
+  children,
+}: {
+  phase: PhaseHierarchy
+  children: React.ReactNode
+}) {
+  return (
+    <div className="w-full p-4 border rounded-lg flex flex-col gap-4">
+      <div className="flex flex-row gap-2 items-center">
+        <TextBody>{`${phase.sequence} ) ${phase.name}`}</TextBody>
+        <Badge>{phase.status}</Badge>
+      </div>
+      <div className="flex flex-row gap-4">{children}</div>
+    </div>
+  )
+}
 
-  const nextAction = useMemo(() => {
-    if (!sortedPhases.length || !sortedSegments.length) return null
-    return determineNextPhaseSegmentStatusAction(sortedPhases, sortedSegments)
-  }, [sortedPhases, sortedSegments, selectedPageant])
+function determinNextAction(phases: Array<PhaseHierarchy>) {
+  return phases
+}
 
-  async function onClick() {
-    /* If all phases and segments are closed, finalize the pageant */
-    if (!nextAction) {
-      await changeState(`/pageants/${selectedPageant?.id}/finalize`)
-      queryClient.invalidateQueries({ queryKey: ['pageants'] })
-      return
-    }
+export default function PageantOngoinDashboard() {
+  const { data: selectedPageant, isLoading: isSelectedPageantLoading } =
+    useSelectedPageant()
+  const { data: pageantHierarchy, isLoading: isPageantHierarchyLoading } =
+    useQuery(
+      pageantHierarchyQueryOptions(selectedPageant!.id, {
+        enabled: !!selectedPageant,
+      }),
+    )
 
-    await changeState(nextAction.endpoint)
-    queryClient.invalidateQueries({ queryKey: ['phases'] })
-    queryClient.invalidateQueries({ queryKey: ['segments'] })
-    console.log(nextAction.endpoint)
+  const PENDING = phaseSegmentStatusValue.enum.PENDING
+  const ONGOING = phaseSegmentStatusValue.enum.ONGOING
+  const CLOSED = phaseSegmentStatusValue.enum.CLOSED
+
+  const startButtonLocation = useMemo(() => {
+    if (!pageantHierarchy) return null
+    return determinNextAction(pageantHierarchy.phases)
+  }, [pageantHierarchy])
+
+  if (isSelectedPageantLoading || isPageantHierarchyLoading) {
+    return (
+      <div className="w-full grid place-items-center">
+        <TextBody>Loading...</TextBody>
+      </div>
+    )
   }
 
   return (
-    <div className="flex flex-row gap-4">
-      <div className="border rounded-lg w-fit h-fit">
-        <div className="px-4 py-4 border-b">
-          <TextBody>Phases and Segments</TextBody>
-        </div>
-        <div className="px-4 pt-4 ">
-          <PhaseSegmentTable phases={sortedPhases} segments={sortedSegments} />
-        </div>
+    <div className="w-full">
+      <div className="flex flex-col gap-4 w-full items-center">
+        {pageantHierarchy?.phases
+          .sort((a, b) => a.sequence - b.sequence)
+          .map((phase) => {
+            return (
+              <PhaseCard phase={phase}>
+                {phase.segments
+                  .sort((a, b) => a.sequence - b.sequence)
+                  .map((segment) => {
+                    const isDisabled = segment.status === PENDING
+
+                    return (
+                      <SegmentCard segment={segment} isDisabled={isDisabled} />
+                    )
+                  })}
+              </PhaseCard>
+            )
+          })}
       </div>
-      {/* <Button onClick={onClick}>
-        {nextAction ? nextAction.label : 'Finalize Pageant'}
-      </Button> */}
-      <ConfirmDialog
-        triggerLabel={nextAction ? nextAction.label : 'Finalize Pageant'}
-        title={nextAction ? nextAction.label : 'Finalize Pageant'}
-        description="This action cannot be undone. Are you sure you want to move to the next stage?"
-        onConfirm={onClick}
-      />
+      <div className="mt-4">
+        <ConfirmDialog
+          triggerLabel={'Finalize Pageant'}
+          title={'Finalize Pageant'}
+          description="This action cannot be undone. Are you sure you want to move to the next stage?"
+          onConfirm={() => {}}
+        />
+      </div>
     </div>
   )
 }
